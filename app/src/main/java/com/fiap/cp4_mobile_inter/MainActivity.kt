e4ef81e51d87bdd883f871f584e77eb3b46f5d90
douglas <douglas.arau@hotmail.com>
2024-09-06 20:25:47 -0300

    Função onclick para mudar para a segunda tela

--- app/src/main/java/com/fiap/cp4_mobile_inter/MainActivity.kt
+++ app/src/main/java/com/fiap/cp4_mobile_inter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fiap.cp4_mobile_inter
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnEntrar: Button = findViewById(R.id.btnEntrar)
+
+        btnEntrar.setOnClickListener{
+            IrPraSegundaTela()
         }
+    }
+    private fun IrPraSegundaTela(){
+        val segundatela = Intent(this, SegundaTela::class.java)
+        startActivity(segundatela)
     }
 }
