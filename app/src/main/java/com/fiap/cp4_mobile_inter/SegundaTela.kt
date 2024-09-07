9dde9a1628197a9835be202c312a2defafde2208
douglas <douglas.arau@hotmail.com>
2024-09-07 20:12:52 -0300

    Quase finalizando CP4

--- app/src/main/java/com/fiap/cp4_mobile_inter/SegundaTela.kt
+++ app/src/main/java/com/fiap/cp4_mobile_inter/SegundaTela.kt
@@ -1,15 +1,27 @@
 package com.fiap.cp4_mobile_inter
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SegundaTela : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_segunda_tela)
 
+        val btnpagethree: Button = findViewById(R.id.btnpagethree)
 
+        btnpagethree.setOnClickListener{
+            IrPraTerceiraTela()
+        }
+    }
+    private fun IrPraTerceiraTela(){
+        val terceiratela = Intent(this, TerceiraTela::class.java)
+        startActivity(terceiratela)
     }
 }
